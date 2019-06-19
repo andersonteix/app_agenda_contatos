b84577ff0b91ec6942b8ed78496a642e14b563ba
anderson <filosofand@gmail.com>
2019-06-18 23:24:59 -0300

    CRUD finalizado

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -49,8 +49,74 @@ class ContactHelper {
     });
   }
 
+  /** Método Salvar contato */
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;   // Obtem o banco de dados
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  /** Método lista um contato */
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;  // Obtem o banco de dados
+    List<Map> maps = await dbContact.query(contactTable,  // Utiliza 'query' para informar qual atributo retornar
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+      where: "$idColumn = ?",
+      whereArgs: [id] );
+
+    if(maps.length > 0){
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  /** Método deleta um contato */
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;  // Obtem o banco de dados
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  /** Método atualiza um contato */
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;  // Obtem o banco de dados
+    return await dbContact.update(contactTable,
+                                  contact.toMap(),
+                                  where: "$idColumn = ?",
+                                  whereArgs: [contact.id]
+                                );
+  }
+
+  /** Método lista com todos contatos */
+  Future<List> getAllContacts() async {
+    Database dbContact = await db;  // Obtem o banco de dados
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContacts = List();
+    for(Map m in listMap){
+      listContacts.add(Contact.fromMap(m));
+    }
+
+    return listContacts;
+  }
+
+  /** Método contabiliza total de contatos */
+  Future<int> getNumber() async {
+    Database dbContact = await db;  // Obtem o banco de dados
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+
+  /** Método fecha conexão com o banco de dados */
+  Future close() async {
+    Database dbContact = await db;  // Obtem o banco de dados
+    dbContact.close();
+  }
 
 }
+
+
+/**
+ * CLASSE CONTACT
+ **/
 
 class Contact {
 
